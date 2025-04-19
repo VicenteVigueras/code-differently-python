2b656647522ccdc9b4af849b71347f35c770c623
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2025-04-19 11:45:10 -0400

    fix: refactor business checking account logic

--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/BusinessCheckingAccount.java
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/BusinessCheckingAccount.java
@@ -4,13 +4,22 @@ import java.util.Set;
 
 public class BusinessCheckingAccount extends CheckingAccount {
 
+  /**
+   * Creates a new Business Checking account.
+   *
+   * @param accountNumber The account number.
+   * @param owners The owners of the account; at least one must be a business account
+   * @param initialBalance The initial balance of the account.
+   */
   BusinessCheckingAccount(String accountNumber, Set<Customer> owners, double initialBalance) {
-    super(accountNumber, owners, initialBalance);
+    super(accountNumber, validateBusinessOwner(owners), initialBalance);
+  }
 
+  private static Set<Customer> validateBusinessOwner(Set<Customer> owners) {
     boolean hasBusinessOwner = owners.stream().anyMatch(Customer::isBusinessAccount);
-
     if (!hasBusinessOwner) {
       throw new IllegalArgumentException("At least one owner must be a business.");
     }
+    return owners;
   }
 }
