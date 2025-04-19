bd6f5f9168332ec973685108cfa9145f9b6de475
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2025-04-19 11:47:55 -0400

    feat: audit log logic and bankatm refactor

--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/AuditLog.java
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/AuditLog.java
@@ -1,3 +1,13 @@
 package com.codedifferently.lesson17.bank;
 
-public class AuditLog {}
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AuditLog {
+
+  private static final Logger logger = (Logger) LoggerFactory.getLogger(AuditLog.class);
+
+  public void record(String record) {
+    logger.info(record);
+  }
+}
