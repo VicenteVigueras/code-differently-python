60e818cc2f2ff3ba2ab9f09799ba248f364c2988
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2025-04-19 11:46:41 -0400

    feat: money order logic

--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/MoneyOrder.java
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/MoneyOrder.java
@@ -1,3 +1,35 @@
 package com.codedifferently.lesson17.bank;
 
-public class MoneyOrder {}
+public class MoneyOrder {
+
+  private String moneyOrderNumber;
+  private double amount;
+  private BankAccount bankAccount;
+
+  /**
+   * Creates MoneyOrder
+   *
+   * @param moneyOrderNumber The Money Order number
+   * @param amount The amount of the money order
+   * @param bankAccount The account the money order is drawn from
+   */
+  public MoneyOrder(String moneyOrderNumber, double amount, BankAccount bankAccount) {
+    this.moneyOrderNumber = moneyOrderNumber;
+    this.amount = amount;
+    this.bankAccount = bankAccount;
+    bankAccount.withdraw(amount);
+  }
+
+  @Override
+  public String toString() {
+    return "MoneyOrder{"
+            + "moneyOrderNumber='"
+            + moneyOrderNumber
+            + '\''
+            + ", amount="
+            + amount
+            + ", account="
+            + bankAccount.getAccountNumber()
+            + '}';
+  }
+}
