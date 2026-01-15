1464771c7a7b8278b460cb272ebd8e268bfb9228
VicenteVigueras <vicente.viguerassilva@gmail.com>
2026-01-14 22:06:49 -0500

    feat: MultipleChoceQuestion, AnswerChoice classes and make_quiz_questions and make_question_0 static methods

--- lesson_02/quiz_python/lesson2.py
+++ lesson_02/quiz_python/lesson2.py
@@ -1,8 +1,44 @@
+from enum import Enum
+
+class AnswerChoice(Enum):
+    UNANSWERED = "UNANSWERED",
+    A = "A",
+    B = "B",
+    C = "C",
+    D = "D",
+
+
+class MultipleChoiceQuestion: 
+        def __init__(self, question_number, question, choices, answer):
+            self.question_number = question_number  
+            self.question = question  
+            self.choices = choices
+            self.answer = answer
+
+
 class Lesson2:
     def run():
         quiz_questions = Lesson2.make_quiz_questions()
         if not quiz_questions:
             print("No quiz questions found.")
             return
-        printer = QuizPrinter()
-        printer.print_quiz(quiz_questions)
+
+    @staticmethod
+    def make_quiz_questions():
+        return [
+            Lesson2.make_question_0()
+        ]
+    
+    @staticmethod
+    def make_question_0():
+        question = "What is the main purpose of version control?"
+        choices = {
+            "A": "To make backups of files",
+            "B": "To keep a record of changes over time",
+            "C": "To delete unnecessary files",
+            "D": "To run code more efficiently"
+        }
+        answer = "A"
+        question_0 = MultipleChoiceQuestion(0, question, choices, answer)
+        return question_0
+    
