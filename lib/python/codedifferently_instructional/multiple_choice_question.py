1a861135c5cb7543acdc71ace31d85247c4d46c0
VicenteVigueras <vicente.viguerassilva@gmail.com>
2026-01-18 18:17:17 -0500

    feat: add typing and encapsulation to multiple choice question

--- lib/python/codedifferently_instructional/multiple_choice_question.py
+++ lib/python/codedifferently_instructional/multiple_choice_question.py
@@ -1,9 +1,14 @@
 class MultipleChoiceQuestion: 
-        def __init__(self, question_number, question, choices, answer):
-            self.question_number = question_number  
-            self.question = question  
-            self.choices = choices
-            self.answer = answer
+    _question_number: int
+    _question: str
+    _choices: dict[str, str]
+    _answer: str
 
-        def get_answer(self):
-            return self.answer
+    def __init__(self, question_number: int, question: str, choices: dict[str, str], answer: str):
+        self._question_number = question_number  
+        self._question = question  
+        self._choices = choices
+        self._answer = answer
+
+    def get_answer(self) -> str:
+        return self._answer
